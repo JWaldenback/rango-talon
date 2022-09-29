687fdf8421999b70f94b6d2c93357a101d01bd44
JWaldenback <joel.waldenback@outlook.com>
2022-09-29 10:15:55 +0200

    change commands + change default scroll value to 0.9

--- rango.py
+++ rango.py
@@ -188,9 +188,11 @@ class UserActions:
     def rango_disable_direct_clicking():
         ctx.tags = []
 
-    #Defining mouse scroll actions here enables for the usage of the exact same voice commands in mouse.py
-    def mouse_scroll_up(amount: float = 1):
+    #Defining mouse scroll actions here enables for the usage of the exact same voice commands in knausj_talons mouse.py but Rango has precedence when a browser is in focus
+    #"""
+    def mouse_scroll_up(amount: float = 0.9):
         actions.user.rango_command_without_target("scrollUpPage", amount)
 
-    def mouse_scroll_down(amount: float = 1):
+    def mouse_scroll_down(amount: float = 0.9):
         actions.user.rango_command_without_target("scrollDownPage", amount)
+    #"""
